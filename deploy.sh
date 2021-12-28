af96b19d5d4fe11438591ce393389f495f90c373
danieljjh <danieljjh@gmail.com>
2021-12-29 00:10:27 +0800

    up

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-scp -r -i /Users/DanielJiang/Documents/ec2/LightsailDefaultKey-us-east-1.pem ./dist ubuntu@100.24.201.150:/home/ubuntu/project/sc-vue 
-# scp -r -i /Users/DanielJiang/Documents/ec2/aws-jy-nx-ubuntu.pem dist ubuntu@161.189.200.186:/home/ubuntu/projects/hotel-web/hotel-back/
-# aws s3 cp ./dist s3://hoteldemo.i568.me --acl public-read --recursive --profile jy-dan
